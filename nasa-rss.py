23e9bafc180f2167b0b8c5ce14ab2a9adb73ca38
Jake Johnson <jake@archive.org>
2012-01-16 20:36:47 +0000

    added faceting functions from Raj

--- nasa-rss.py
+++ nasa-rss.py
@@ -16,7 +16,7 @@ rootDir = os.getcwd()
 logging.config.fileConfig('logging.conf')
 cLogger = logging.getLogger('console')
 
-def getFeedList():
+def get_feed_list():
     masterFeedList = "http://www.nasa.gov/rss/internetarchive/index.html"
     parsed = lxml.html.fromstring(urllib.urlopen(masterFeedList).read())
     feedList =  ([link[2] for link in parsed.iterlinks() if link[2].endswith(
@@ -24,7 +24,7 @@ def getFeedList():
     feedList = list(set(feedList))
     return feedList
 
-def checkArchive(identifier):
+def check_archive(identifier):
     url = "http://www.archive.org/services/check_identifier.php?identifier="
     retMessage = etree.parse(url + identifier).getroot().findtext('message')
     if retMessage == 'The identifier you have chosen is available': 
@@ -37,20 +37,55 @@ def mkdir(dirname):
         os.mkdir(dirname)                                                                               
     os.chdir(dirname)        
 
-def facet_dict(string):
-    facet_file = '%s/facets.txt' % (rootDir)
+def build_dict():
+    facet_file = '/home/jake/facets.txt'
     facet_list = open(facet_file,'rb').read().split('\n')
     dictionary = {}
+    max_words_in_key = 0
     for facet in facet_list:
         k,v = facet.split(',')[0], facet.split(',')[-1]
-        dictionary[k] = v
+        k = k.strip().lower()
+        if not k:
+            continue
+        words_in_key = len(k.split(' '))
+        if words_in_key > max_words_in_key:
+            max_words_in_key = words_in_key
+        dictionary[k] = v.strip()
+    return dictionary, max_words_in_key
+
+def get_phrase(words, phrase_length, start_pos):
+    s = ''
+    for i in range(phrase_length):
+        s += words[start_pos+i] + ' '
+        exclude =set(['!', '#', '"', '%', '$', "'", '&', ')', '(', '+', '*', ',',
+                      '/', '.', ';', ':', '=', '<', '?', '>', '@', '[', ']', '\\',
+                      '^', '`', '{', '}', '|', '~'])
+        s = ''.join(ch for ch in s if ch not in exclude)
+    return s[:-1]
+
+def get_facets(string, dictionary, longest_key):
+    # Extract facets from string
     faceted = {}
-    for k,v in dictionary.iteritems():
-        if k in string:
-            faceted[k] = v
+    words = string.split()
+    num_words = len(words)
+    pos = 0
+    while pos < num_words:
+        phrase_length = min(longest_key, num_words-pos)
+        found_phrase = False
+        while phrase_length > 0:
+            phrase = get_phrase(words, phrase_length, pos)
+            if phrase.lower() in dictionary:
+                found_phrase = phrase.lower()
+                break
+            phrase_length -= 1
+        if False != found_phrase:
+            faceted[found_phrase] = dictionary[found_phrase]
+            pos += phrase_length
+        else:
+            pos += 1
     return faceted
 
-def makeMeta(metaDict):                                                                                 
+def make_meta(metaDict):                                                                                 
     # Generate {item}_files.xml stub and {item}_meta.xml from metaDict
     f = open("%s_files.xml" % metaDict['identifier'], "wb")                                             
     f.write("</files>")                                                                                 
@@ -61,6 +96,7 @@ def makeMeta(metaDict):
         subElement.text = v                                                                             
     metaXml = etree.tostring(root, pretty_print=True,                                                   
                              xml_declaration=True, encoding="utf-8")                                    
+    print metaXml
     ff = open("%s_meta.xml" % metaDict['identifier'], "wb")                                             
     ff.write(metaXml)                                                                                   
     ff.close()
@@ -72,7 +108,7 @@ def wget(mediaLink):
 def main():                                                                  
     mkdir('/1/incoming/tmp/nasa-rss')    
     home = os.getcwd()            
-    for feed in getFeedList():
+    for feed in get_feed_list():
         parsed = feedparser.parse(feed)                                                                 
         if parsed.bozo == 1: logging.warning('%s is a bozo!' % feed)                                    
         for entry in parsed.entries:                                                                    
@@ -81,7 +117,7 @@ def main():
                 identifier = ( entry.media_content[0]['url'].split('/')
                                [-1].split('.')[0] )
                 metaDict['identifier'] = identifier.replace('_full','')
-                if checkArchive(metaDict['identifier']) != 0: 
+                if check_archive(metaDict['identifier']) != 0: 
                     cLogger.info('the identifier "%s" is not available' % 
                                  metaDict['identifier'] )
                     continue                                  
@@ -99,17 +135,16 @@ def main():
                 facet_string = '%s %s %s' % (metaDict['description'],
                                              metaDict['title'],
                                              entry['media_keywords'])
-                fdict = facet_dict(facet_string)
-                print fdict
-                fl = []
-                for k,v in fdict.iteritems():
-                    if k:
-                        fl.append(('%s %s' % (v,k)))
-                if fl:
-                    metaDict['subject'] = ';'.join(fl)
+                facet_dict, logest_key = build_dict()
+                facets = get_facets(facet_string, facet_dict, logest_key)
+                facet_list = []
+                for v in facets.itervalues():
+                    facet_list.append(v)
+                if facet_list:
+                    metaDict['subject'] = ';'.join(facet_list)
 
-#                makeMeta(metaDict)
-#                wget(entry.media_content[0]['url'])
+                make_meta(metaDict)
+                wget(entry.media_content[0]['url'])
             except AttributeError:                                                                      
                 noMedia = ("%s doesn't appear to have any media!" % 
                            entry.links[0].href)                 
